5c483cd70b64fcb3f95effbc561fd69f74305ec2
Haider Majid <92584858+haider-majid@users.noreply.github.com>
2025-02-16 10:31:48 +0300

    - fix response

---
+++ Migrations/20250216072415_updateLocationName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace clean_architecture.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateLocationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Loaction",
+                table: "users",
+                newName: "Location");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "users",
+                newName: "Loaction");
+        }
+    }
+}
