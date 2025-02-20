a2d7739b737d4528d22ebd779a6d230fd76bd93a
Haider Majid <92584858+haider-majid@users.noreply.github.com>
2025-02-20 22:14:13 +0300

    - update

--- obj/Debug/net8.0/clean architecture.AssemblyInfo.cs
+++ obj/Debug/net8.0/clean architecture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clean architecture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6dfb71d675c92f05148a7c9714de3fa6105e7ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6715368575debbe203d09f7d0d4061063aa45eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("clean architecture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clean architecture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
